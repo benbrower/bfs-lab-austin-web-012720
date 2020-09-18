a98e01828d243e56ec6effe24b5d3534282a61af
Ben Brower <bbrower.dt@gmail.com>
2020-09-18 19:12:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
-function bfs(rootNode, vertices, edges){
-  rootNode.distance = 0;
-  let queue = [rootnode]
-  let order = [rootNode];
 
-  while (queue.length != 0){
-    let currentNode = queue.shift();
-    let adjacentNodes = findAdjacent(currentNode.name, vertices, edges);
-    order = order.concat(adjacentNodes);
-    markDistanceAndPredecessor(currentNode, AdjacentNodes);
-    queue = queue.concat(adjacentNodes);
+function bfs(startingNode, vertices, edges){
+  startingNode.distance = 0
+  let discovered = [startingNode]
+  let discoverOrder = [startingNode]
+  while(discovered.length != 0){
+    let currentNode = discovered.shift()
+    let adjacentNodes = findAdjacent(currentNode.name, vertices, edges)
+    discoverOrder = discoverOrder.concat(adjacentNodes);
+    markDistanceAndPredecessor(currentNode, adjacentNodes)
+    discovered = discovered.concat(adjacentNodes)
   }
-  return order;
+  return discoverOrder
 }
 
-//returns adjacent nodes
-function findAdjacent(name, vertices, queue){
-  return edges.filter(function(edge) {
-    return edge.includes(name)
+// need to write a test for non-discovered nodes
+function findAdjacent(nodeName,  vertices, edges){
+  return edges.filter(function(edge){
+    return edge.includes(nodeName)
   }).map(function(edge){
     return edge.filter(function(node){
-      return (node != name)
+      return (node != nodeName)
     })[0]
   }).map(function(name){
     return findNode(name, vertices)
   }).filter(function(node){
-    return node.distance==null;
+    return node.distance == null;
   })
 }
 
-//updates node with its predecessor node and distance from root
 function markDistanceAndPredecessor(predecessor, adjacentNodes){
   adjacentNodes.map(function(node){
     node.distance = predecessor.distance + 1;
