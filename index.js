dec7958bf6976063bb09ac4cea6bc5139253ea9a
Ben Brower <bbrower.dt@gmail.com>
2020-09-18 19:07:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,13 +7,15 @@ function bfs(rootNode, vertices, edges){
     let currentNode = queue.shift();
     let adjacentNodes = findAdjacent(currentNode.name, vertices, edges);
     discoverOrder = discoverOrder.concat(adjacentNodes);
+    markDistanceAndPredecessor(currentNode, AdjacentNodes);
+    queue = queue.concat(adjacentNodes);
   }
-
+  return discoverOrder;
 }
 
 //returns adjacent nodes
 function findAdjacent(node, vertices, queue){
-
+  return
 }
 
 //updates node with its predecessor node and distance from root
